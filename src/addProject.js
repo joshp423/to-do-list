0984c5a07e428c0f584c26eee73d1e0a3d759fab
joshp423 <205880399+joshp423@users.noreply.github.com>
2025-09-25 18:14:19 +1200

    add project WIP

---
+++ src/addProject.js
@@ -0,0 +1,7 @@
+import { userProjects } from "./initialArrays";
+import { Project } from "./projectClass";
+
+export function addProject(){
+    const title = document.get
+    userProjects.push(new Project())
+}
